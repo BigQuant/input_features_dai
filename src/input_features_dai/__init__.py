4c770248851e8cbeed10001437b08bf7446fc477
Ju Liang <jliang@bigquant.ai>
2024-04-27 20:10:20 +0800

    BQ-525 expr mode: support filtering

--- src/input_features_dai/__init__.py
+++ src/input_features_dai/__init__.py
@@ -1,4 +1,4 @@
-"""input_features_dai 模块输入 SQL，可用于因子和特征抽取、数据标注等"""
+"""input_features_dai 模块输入 SQL, 可用于因子和特征抽取、数据标注等"""
 
 import re
 import uuid
@@ -30,15 +30,20 @@ MODES = OrderedDict(
 )
 MODE0 = list(MODES.keys())[0]
 
-DEFAULT_SQL = """-- 使用DAI SQL获取数据，构建因子等，如下是一个例子作为参考
+DEFAULT_SQL = """-- 使用DAI SQL获取数据, 构建因子等, 如下是一个例子作为参考
 -- DAI SQL 语法: https://bigquant.com/wiki/doc/dai-PLSbc1SbZX#h-sql%E5%85%A5%E9%97%A8%E6%95%99%E7%A8%8B
 -- 使用数据输入1/2/3里的字段: e.g. input_1.close, input_1.* EXCLUDE(date, instrument)
 
 SELECT
-
     -- 在这里输入因子表达式
     -- DAI SQL 算子/函数: https://bigquant.com/wiki/doc/dai-PLSbc1SbZX#h-%E5%87%BD%E6%95%B0
     -- 数据&字段: 数据文档 https://bigquant.com/data/home
+
+    m_lag(close, 90) / close AS return_90,
+    m_lag(close, 30) / close AS return_30,
+    -- 下划线开始命名的列是中间变量, 不会在最终结果输出 (e.g. _rank_return_90)
+    c_pct_rank(-return_90) AS _rank_return_90,
+    c_pct_rank(return_30) AS _rank_return_30,
 
     c_rank(volume) AS rank_volume,
     close / m_lag(close, 1) as return_0,
@@ -47,31 +52,51 @@ SELECT
     date, instrument
 FROM
     -- 预计算因子 cn_stock_bar1d https://bigquant.com/data/datasources/cn_stock_bar1d
-    cn_stock_bar1d
+    cn_stock_factors
+    -- SQL 模式不会自动join输入数据源, 可以根据需要自由灵活的使用
+    -- JOIN input_1 USING(date, instrument)
 WHERE
-    -- WHERE 过滤，在窗口等计算算子之前执行
+    -- WHERE 过滤, 在窗口等计算算子之前执行
     -- 剔除ST股票
     st_status = 0
 QUALIFY
-    -- QUALIFY 过滤，在窗口等计算算子之后执行，比如 m_lag(close, 3) AS close_3，对于 close_3 的过滤需要放到这里
+    -- QUALIFY 过滤, 在窗口等计算算子之后执行, 比如 m_lag(close, 3) AS close_3, 对于 close_3 的过滤需要放到这里
     -- 去掉有空值的行
     COLUMNS(*) IS NOT NULL
--- 按日期和股票代码排序，从小到大
+    -- _rank_return_90 是窗口函数结果，需要放在 QUALIFY 里
+    AND _rank_return_90 > 0.1
+    AND _rank_return_30 < 0.1
+-- 按日期和股票代码排序, 从小到大
 ORDER BY date, instrument
 """
 
 DEFAULT_EXPR = """-- DAI SQL 算子/函数: https://bigquant.com/wiki/doc/dai-PLSbc1SbZX#h-%E5%87%BD%E6%95%B0
 -- 数据&字段: 数据文档 https://bigquant.com/data/home
--- 数据使用: 表名.字段名, 对于没有指定表名的列，会从 expr_tables 推断
--- 给输出数据列命名: AS field_name
--- 使用数据输入1/2/3里的字段: e.g. input_1.close, input_1.* EXCLUDE(date, instrument)
--- 在这里输入表达式, 每行一个表达式, 会根据这个输入解析表名并构建查询和计算SQL,
+-- 数据使用: 表名.字段名, 对于没有指定表名的列, 会从 expr_tables 推断, 如果同名字段在多个表中出现, 需要显式的给出表名
 
-cn_stock_bar1d.close / cn_stock_bar1d.open AS daily_change
-c_rank(cn_stock_valuation.total_market_cap) AS rank_total_market_cap
-m_lag(rank_total_market_cap, 2) AS rank_total_market_cap_2
+m_lag(close, 90) / close AS return_90
+m_lag(close, 30) / close AS return_30
+-- cn_stock_bar1d.close / cn_stock_bar1d.open
+-- cn_stock_factors https://bigquant.com/data/datasources/cn_stock_factors 是常用因子表(VIEW), JOIN了很多数据表, 性能会比直接用相关表慢一点, 但使用简单
+-- cn_stock_factors.pe_ttm
+
+-- 表达式模式下, 会自动join输入数据1/2/3, 可以在表达式里直接使用其字段。包括 input_1 的所有列但去掉 date, instrument。注意字段不能有重复的, 否则会报错
+-- input_1.* EXCLUDE(date, instrument)
+-- input_1.close
+-- input_2.close / input_1.close
+
+-- date, instrument 需要显式的给出
 date
 instrument
+"""
+
+DEFAULT_EXPR_FILTERS = """-- DAI SQL 算子/函数: https://bigquant.com/wiki/doc/dai-PLSbc1SbZX#h-%E5%87%BD%E6%95%B0
+-- 数据&字段: 数据文档 https://bigquant.com/data/home
+-- 表达式模式的过滤都是放在 QUALIFY 里, 即数据查询、计算, 最后才到过滤条件
+
+-- c_pct_rank(-return_90) <= 0.3
+-- c_pct_rank(return_30) <= 0.3
+-- cn_stock_bar1d.turn > 0.02
 """
 
 TABLE_NAME_RE = re.compile(r"(?<!\.)\b[a-zA-Z_]\w*\b(?=\.[a-zA-Z_*])")
@@ -85,7 +110,7 @@ FROM {tables}
 """
 
 
-def _build_table(ds) -> dict:
+def _ds_to_table(ds) -> dict:
     if isinstance(ds, str):
         sql = ds
     else:
@@ -113,7 +138,24 @@ def _build_table(ds) -> dict:
     }
 
 
-def _build_sql_from_expr(expr: str, default_tables="", order_by="", expr_drop_na=True, multi_expr=True):
+def _ds_to_tables(inputs) -> dict:
+    sql = ""
+    tables = []
+    input_tables = []
+    for i, x in enumerate(inputs):
+        if x is None:
+            continue
+        table = _ds_to_table(x)
+        table["name"] = f"input_{i+1}"
+        tables.append(table)
+
+    return {
+        "items": tables,
+        "sql": "".join([x["sql"] for x in tables])
+    }
+
+
+def _split_expr(expr):
     lines = []
     for line in expr.splitlines():
         line = line.strip()
@@ -122,65 +164,49 @@ def _build_sql_from_expr(expr: str, default_tables="", order_by="", expr_drop_na
         if line.startswith("--") or line.startswith("#"):
             continue
         lines.append(line)
-    if not multi_expr:
-        lines = " ".join(lines)
 
-    exprs = []
-    tables = []
-    if default_tables:
-        if isinstance(default_tables, str):
-            default_tables = [x.strip() for x in default_tables.split(",") if x.strip()]
-        tables += default_tables
-    for line in lines:
-        exprs.append(line)
+    return lines
+
+
+def _build_sql_from_expr(expr: str, expr_filters: str, default_tables="", order_by="", expr_drop_na=True, input_tables={}):
+    expr_lines = _split_expr(expr)
+    filter_lines = _split_expr(expr_filters)
+
+    # collect all tables, join them
+    tables = [x.strip() for x in default_tables.split(",") if x.strip()] + [x["table_id"] for x in input_tables["items"]]
+    for line in expr_lines + filter_lines:
         tables += TABLE_NAME_RE.findall(line)
+    # de-dup and add using primary key
+    join_usings = {}
+    table_set = set()
+    table_list = []
+    for x in tables:
+        if " USING(" in x:
+            s = x.split(" ", 1)
+            join_usings[s[0]] = s[1]
+            x = s[0]
+        # TODO: process x is input_*
+        if x not in table_set:
+            table_list.append(x)
+        table_set.add(x)
+    for i in range(1, len(table_list)):
+        table_list[i] += join_usings.get(table_list[i], " USING(date, instrument)")
+    tables="\n    JOIN ".join(table_list)
 
-    tables = list(sorted(set(tables)))
-    for i in range(1, len(tables)):
-        if " USING(" not in tables[i]:
-            tables[i] = f"{tables[i]} USING(date, instrument)"
-
-    qualify = ""
+    # 构建过滤添加，放到 QUALIFY 里
     if expr_drop_na:
-        qualify = "QUALIFY\n    COLUMNS(*) IS NOT NULL"
+        filter_lines.append("COLUMNS(*) IS NOT NULL")
+    qualify = ""
+    if filter_lines:
+        qualify = "QUALIFY\n    " + "\n    AND ".join(filter_lines)
 
     # ORDER BY date, instrument
     if order_by:
         order_by = f"ORDER BY {order_by}"
-    return EXPR_SQL_TEMPLATE.format(expr=",\n    ".join(exprs), tables="\n    JOIN ".join(tables), qualify=qualify, order_by=order_by)
 
+    sql = EXPR_SQL_TEMPLATE.format(expr=",\n    ".join(expr_lines), tables=tables, qualify=qualify, order_by=order_by)
 
-def _process_inputs(sql, *inputs):
-    create_table_sql = ""
-    for i, x in enumerate(inputs):
-        if x is None:
-            continue
-        table = _build_table(x)
-        create_table_sql += table["sql"]
-        sql = re.sub(rf"\binput_{i+1}\b", table["table_id"], sql)
-
-    return create_table_sql + sql
-
-
-def _ds_passthourgh(ds, do_log=True):
-    # e.g. 上游数据抽取模块的start/end date，bigtrader会根据这个时间确定回测开始结束时间
-    import dai
-
-    data = {}
-
-    if ds is not None and isinstance(ds, dai.DataSource):
-        try:
-            extra = ds.metadata.get("extra", "")
-            if extra:
-                data["extra"] = extra
-        except:
-            # in case there's no metadata
-            pass
-
-    if do_log and data:
-        logger.info(f"ds passthourgh {data}")
-
-    return data
+    return input_tables["sql"] + sql
 
 
 def _create_ds_from_sql(sql: str, extract_data: bool, base_ds=None):
@@ -202,36 +228,46 @@ def _create_ds_from_sql(sql: str, extract_data: bool, base_ds=None):
 
 
 def run(
-    input_1: I.port("数据输入1，如果有metadata extra，会传递给输出 data", specific_type_name="DataSource", optional=True) = None,
+    input_1: I.port("数据输入1, 如果有metadata extra, 会传递给输出 data", specific_type_name="DataSource", optional=True) = None,
     input_2: I.port("数据输入2", specific_type_name="DataSource", optional=True) = None,
     input_3: I.port("数据输入3", specific_type_name="DataSource", optional=True) = None,
     mode: I.choice("输入模式", list(MODES.keys())) = MODE0,
     expr: I.code(
-        "表达式特征，通过表达式构建特征，简单易用",
+        "表达式特征, 通过表达式构建特征, 简单易用",
         default=DEFAULT_EXPR,
         auto_complete_type="sql",
     ) = None,
-    expr_tables: I.str("表达式-默认数据表，对于没有给出表名的字段，默认来自这些表，只填写需要的表，可以提高性能，多个表名用英文逗号分隔") = "cn_stock_factors",
-    order_by: I.str("表达式-排序字段，排序字段 e.g. date ASC, instrument DESC") = "date, instrument",
-    expr_drop_na: I.bool("表达式-移除空值，去掉包含空值的行，用于表达式模式的参数") = True,
+    expr_filters: I.code(
+        "表达式过滤条件, 每行一个条件, 多个条件之间是 AND 关系, 条件内可以使用 OR 组合",
+        default=DEFAULT_EXPR_FILTERS,
+        auto_complete_type="sql",
+    ) = None,
+    expr_tables: I.str("表达式-默认数据表, 对于没有给出表名的字段, 默认来自这些表, 只填写需要的表, 可以提高性能, 多个表名用英文逗号分隔") = "cn_stock_factors",
+    order_by: I.str("表达式-排序字段, 排序字段 e.g. date ASC, instrument DESC") = "date, instrument",
+    expr_drop_na: I.bool("表达式-移除空值, 去掉包含空值的行, 用于表达式模式的参数") = True,
     sql: I.code(
-        "SQL特征，通过SQL来构建特征，更加灵活，功能最全面",
+        "SQL特征, 通过SQL来构建特征, 更加灵活, 功能最全面",
         default=DEFAULT_SQL,
         auto_complete_type="sql",
     ) = None,
-    extract_data: I.bool("抽取数据，是否抽取数据，如果抽取数据，将返回一个BDB DataSource，包含数据DataFrame") = False,
+    extract_data: I.bool("抽取数据, 是否抽取数据, 如果抽取数据, 将返回一个BDB DataSource, 包含数据DataFrame") = False,
 ) -> [I.port("输出(SQL文件)", "data")]:
     """输入特征（因子）数据"""
+
+    input_tables = _ds_to_tables([input_1, input_2, input_3])
+
     mode = MODES[mode]
-    if mode == "sql":
-        logger.info("sql mode")
-    else:
+    if mode == "expr":
         logger.info("expr mode")
         if "date" not in expr or "instrument" not in expr:
             logger.warning("not found date/instrument in expr, the new version will not add date, instrument by default")
-        sql = _build_sql_from_expr(expr, expr_tables, order_by=order_by, expr_drop_na=expr_drop_na, multi_expr=True)
+        sql = _build_sql_from_expr(expr, expr_filters, expr_tables, order_by=order_by, expr_drop_na=expr_drop_na, input_tables=input_tables)
+    else:
+        logger.info("sql mode")
 
-    sql = _process_inputs(sql, input_1, input_2, input_3)
+    # 替换 input_*
+    for x in input_tables:
+        sql = re.sub(rf"\b{x[0]}\b", x[1], sql)
 
     # 使用第一个input ds的 extra
     return I.Outputs(data=_create_ds_from_sql(sql, extract_data, input_1))
