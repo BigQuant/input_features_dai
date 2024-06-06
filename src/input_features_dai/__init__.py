688581c9fb738f6d15198e688ebd387691662415
jwu <jwu@bigquant.ai>
2024-06-06 15:55:11 +0800

    AIOS-2506 fit in new 'I.code'

--- src/input_features_dai/__init__.py
+++ src/input_features_dai/__init__.py
@@ -272,7 +272,8 @@ def run(
         # if "date" not in expr or "instrument" not in expr:
         #     logger.warning("not found date/instrument in expr, the new version will not add date, instrument by default")
         if expr is None:
-            raise ValueError("expr 模式下， 表达式特征输入为空！")
+            logger.error("expr 模式下， 表达式特征输入为空")
+            raise
 
         if expr_filters is None:
             expr_filters = ""
@@ -285,7 +286,8 @@ def run(
     else:
         logger.info("sql mode")
         if sql is None:
-            raise ValueError("SQL 模式下， SQL特征输入为空！")
+            logger.error("sql 模式下， SQL特征输入为空！")
+            raise
         final_sql = sql
 
     # 替换 input_*
